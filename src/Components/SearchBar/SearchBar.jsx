b4246bda73499e73dadc5ab3dd9fead68831bb34
Rosy <rosy24dr@outlook.com>
2022-05-16 14:05:58 -0400

    created the clearSearch function

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -2,34 +2,28 @@ import React, { useState } from 'react';
 
 const SearchBar = (props) => {
     // After Search Happens, Get Error Code Because now Games List is that filter List from search
-    // const [userInput, setUserInput] = useState("");
+    const [userInput, setUserInput] = useState("");
 
-    // function handleSubmit(event) {
-    //     event.preventDefault();
-    //     let filteredGames = props.games.filter((game) => {
-    //         if (game.name.includes(userInput)) { return true };
-    //     })
-    // props.setGames(filteredGames)
-    // }
-
-
-    const [searchGames, setSearchGames] = useState('')
-
-    let handleSearch = (event) => {
-        let searchedGame = event.target.value
-        setSearchGames(searchedGame)
-        props.searchVideoGames(searchedGame)
+    function handleSubmit(event) {
+        event.preventDefault();
+        let filteredGames = props.games.filter((game) => {
+            if (game.name.toLowerCase().includes(userInput.toLowerCase())) { 
+                return true; 
+        }})
+        props.setGames(filteredGames)
+        setUserInput("")
     }
 
-
-
-
+    function clearSearch(event){
+        event.preventDefault();
+        props.getAllVideoGames();
+    }
     return (
-        <form onSubmit= {handleSearch}>
+        <form onSubmit={handleSubmit}>
 
-            <input value={searchGames} onChange={handleSearch} />
+            <input value={userInput} onChange={(e) => setUserInput(e.target.value)} />
             <button type="submit" className="btn btn-primary">Search</button>
-
+            <button type="button" onClick = {clearSearch}>Clear</button>
         </form>
     );
 }
